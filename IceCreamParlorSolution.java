d6cff7c4cf87e9c0f894a29602ba5469380be529
chlohal <me@clh.sh>
2023-10-23 17:38:36 -0400

    Make version that uses textbook backtracking

--- IceCreamParlorSolution.java
+++ IceCreamParlorSolution.java
@@ -22,15 +22,7 @@ class IceCreamParlorResult {
      */
 
     public static List<Integer> icecreamParlor(int m, List<Integer> arr) {
-        //unassigned friends are represented by `-1`. Start the flavour indexes
-        //by marking all 3 friends as unassigned. 
-        int[] baseIndexes = new int[] {-1, -1, -1};
-        
-        //start the result with all -1, just so it'll have a `sumIndexValues(...)` of 0.
-        int[] result = new int[] {-1, -1, -1};
-
-        //this will mutate `baseIndexes` to hold the best possible solution
-        findClosestBranchToTarget(baseIndexes, result, arr, m);
+        int[] result = findBestPathToTarget(arr, m, new int[0]);
         
         ArrayList<Integer> listResult = new ArrayList<>();
         
@@ -38,74 +30,36 @@ class IceCreamParlorResult {
         
         return listResult;
     }
-    
-    /**
-    Main recursive function
-    currentIndexes: governs where we are in the tree of possibilities.
-    bestCandidateSoFar: the best leaf of the tree. Whenever a leaf is reached, it's checked,
-        and if it surpasses, bestCandidateSoFar is overwritten
-    amounts: the list of flavour prices
-    target: the amount of money to spend
-    */
-    private static void findClosestBranchToTarget(int[] currentIndexes, int[] bestCandidateSoFar, List<Integer> amounts, int target) {
-        //open slots (i.e. unassigned friends) are represented by `-1`. Find the 
-        //first unassigned friend's index. If all friends are assigned to a flavour,
-        //then this will be `-1`.
-        int firstUnfilledIndex = findFirstIndexOfNegative(currentIndexes);
-        
-        //if all indexes are filled, then don't bother to search branches from this 
-        //possibility, since there aren't any!
-        //instead, evaluate the leaf of indexes we've found ourselves in
-        //this is the base case.
-        if(firstUnfilledIndex == -1) {
-            //if this leaf is better than the best candidate:
-            if(sumIndexValues(currentIndexes, amounts) > sumIndexValues(bestCandidateSoFar, amounts)) {
-                // overwrite the best candidate with this leaf
-                arrayAssign(bestCandidateSoFar, currentIndexes);
+
+
+    public static int[] findBestPathToTarget(List<Integer> amounts, int target, int[] previousIndexes) {
+        if(previousIndexes.length == 3) return previousIndexes;
+
+        int[] maxChoicePath = new int[0];
+        int maxChoiceValue = 0;
+        for(int i = 0; i < amounts.size(); i++) {
+            if(contains(previousIndexes, i)) continue;
+            if(sumIndexValues(previousIndexes, amounts) + amounts.get(i) > target) continue;
+
+            int[] branch = findBestPathToTarget(amounts, target, arrayWithAddition(previousIndexes, i));
+
+            if(sumIndexValues(branch, amounts) > maxChoiceValue) {
+                maxChoiceValue = sumIndexValues(branch, amounts);
+                maxChoicePath = branch;
             }
-            //and regardless of how good this leaf is, early return
-            return;
-        };
-        
-        //search all other possibilities 
-        for(int flavourIndex = 0; flavourIndex < amounts.size(); flavourIndex++) {
-            //if a previous friend has this flavour, continue, since 
-            //selecting the same flavour breaks the 'distinct flavour'
-            //requirement
-            if(contains(currentIndexes, flavourIndex)) continue;
-            
-            //check if accepting this flavour would be too expensive 
-            //if so, continue
-            if(sumIndexValues(currentIndexes, amounts) + amounts.get(flavourIndex) > target) continue;
-            
-            //tentatively accept this flavour and check further branches down the tree
-            //after recursing, `bestCandidateSoFar` holds the largest legal (not too expensive, distinct)
-            //possible branch.
-            currentIndexes[firstUnfilledIndex] = flavourIndex;
-            findClosestBranchToTarget(currentIndexes, bestCandidateSoFar, amounts, target);
-            //empty the current friend's slot for the next iteration of the loop
-            currentIndexes[firstUnfilledIndex] = -1;
         }
-        
-        //after looping, `bestCandidateSoFar` holds the largest legal branch over all possibilities
+
+        return maxChoicePath;
     }
-    
-    //helper method to copy from one array to another. 
-    //arrays must have same length
-    private static void arrayAssign(int[] target, int[] source) {
-        for(int i = 0; i < target.length; i++) {
-            target[i] = source[i];
-        }
+
+    //helper method to add an integer to the end of an array
+    private static int[] arrayWithAddition(int[] base, int addition) {
+        int[] result = new int[base.length + 1];
+        System.arraycopy(base, 0, result, 0, base.length);
+        result[result.length - 1] = addition;
+        return result;
     }
-    
-    //helper method to find the first index of a negative value
-    private static int findFirstIndexOfNegative(int[] a) {
-        for(int i = 0; i < a.length; i++) {
-            if(a[i] < 0) return i;
-        }
-        return -1;
-    }
-    
+
     //helper method to sum values, given an array of indexes into a list of amounts
     private static int sumIndexValues(int[] indexes, List<Integer> amounts) {
         int sum = 0;
